05f63506619e5375f5a8bcf6d7a2107d22c98939
CarlaOnate <carlaonategardella@gmail.com>
2020-02-03 22:24:45 -0600

    Medio lijto

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,15 +3,19 @@ require('dotenv').config();
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
+const session = require("express-session");
 const favicon      = require('serve-favicon');
 const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const flash = require("connect-flash");
+const passport = require('passport')
+
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/authTest', {useNewUrlParser: true, useUnifiedTopology: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -29,6 +33,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Express View engine setup
 
@@ -37,7 +52,7 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -55,6 +70,7 @@ const index = require('./routes/index');
 app.use('/', index);
 const passportRouter = require("./routes/passportRouter");
 app.use('/', passportRouter);
+//hace middleware para checar session
 
 
 module.exports = app;
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,14 +1,26 @@
 const express        = require("express");
 const passportRouter = express.Router();
-
+const {signup, signupView, loginView, login} = require('../contollers/authController')
 // Require User model
+const User = require('../models/User')
+const passport = require('../config/passport')
+
+
 
 // Signup Route
+passportRouter.get('/signup', signupView)
+passportRouter.post('/signup', signup)
 
 // Login Route
+passportRouter.get('/login', loginView)
+passportRouter.post('/login', passport.authenticate('local', {
+  successRedirect: '/profile',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
 
 // Logout Route
-
 passportRouter.get("/private-page", ensureLogin, (req, res) => {
   res.render("passport/private", { user: req.user });
 });
