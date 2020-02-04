1b6a843b4c667a548013cb4c78939859c798ce10
Edith LE <edith.lop.estrada@gmail.com>
2020-02-04 10:49:23 -0600

    ya resuelto el login, revisando que entre a la base de datos

--- starter-code/controllers/index.js
+++ starter-code/controllers/index.js
@@ -5,18 +5,23 @@ exports.signupView = (req,res) =>{
 }
 
 exports.signup = async(req, res) =>{
-  const {name, email, password} = req.body
+  const {email, password} = req.body
   if(email === "" || password === ""){
     res.render('passport/signup', {
       message: "Completa los campos"
     })
   }
-  const userOnDB =await User.findOne({email});
-  if (userOnDB !== null){
-    res.render('auth/signup', {message: 'El correo ya fue registrado'})
+  const userNot =await User.findOne({email});
+  if (userNot !== null){
+    res.render('passport/signup', {message: 'El correo ya fue registrado'})
   }
-  await User.register({name, email}, password)
+  await User.register({email}, password)
   res.redirect('/login')
 }
+
+exports.loginView = (req, res) =>{
+  res.render('passport/login')
+}
+
 
 
