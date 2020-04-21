7d3b145b7e41ce6d3a9cb84ba94e2ffc79ec1013
FranciscoEP <francisco.eugp@gmail.com>
2020-04-20 19:55:58 -0500

    missing styles

--- starter-code/models/User.js
+++ starter-code/models/User.js
@@ -1,14 +1,16 @@
-const mongoose = require("mongoose");
-const Schema   = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const PLM = require('passport-local-mongoose')
 
 const userSchema = new Schema(
   {
-    email: String
-  }, 
+    email: String,
+  },
   {
-    timestamps: true
+    timestamps: true,
   }
-);
+)
 
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+userSchema.plugin(PLM, { usernameField: 'email' })
+const User = mongoose.model('User', userSchema)
+module.exports = User
