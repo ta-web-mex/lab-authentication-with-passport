efaabd3cb4f21c8d75d09ec7626cf21be27a2faa
Vidal Diaz <vgdiaz@hotmail.com>
2020-04-20 23:02:13 -0500

    Se realizan las iteraciones solicitadas, sin estilos y sin validaciones adicionales

--- starter-code/models/User.js
+++ starter-code/models/User.js
@@ -1,14 +1,20 @@
-const mongoose = require("mongoose");
-const Schema   = mongoose.Schema;
+const mongoose = require('mongoose')
+const passportConfig = require('passport-local-mongoose')
+const Schema = mongoose.Schema
 
 const userSchema = new Schema(
   {
-    email: String
-  }, 
+    email: {
+      type: String,
+      unique: true,
+    },
+  },
   {
-    timestamps: true
+    timestamps: true,
   }
-);
+)
 
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+userSchema.plugin(passportConfig, { usernameField: 'email' })
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,20 +1,31 @@
-const express        = require("express");
-const passportRouter = express.Router();
+const express = require('express')
+const passportRouter = express.Router()
 
-// Require User model
+const {
+  showSignUp,
+  saveSignUp,
+  showLogin,
+  createLogin,
+  doneLogin,
+} = require('../controllers/passportRouter')
 
 // Signup Route
+passportRouter.get('/signup', showSignUp)
+passportRouter.post('/signup', saveSignUp)
 
 // Login Route
+passportRouter.get('/login', showLogin)
+passportRouter.post('/login', createLogin)
 
+passportRouter.get('/done', doneLogin)
 // Logout Route
 
-passportRouter.get("/private-page", ensureLogin, (req, res) => {
-  res.render("passport/private", { user: req.user });
-});
+passportRouter.get('/private-page', ensureLogin, (req, res) => {
+  res.render('passport/private', { user: req.user })
+})
 
 function ensureLogin(req, res, next) {
-  return req.isAuthenticated() ? next() : res.redirect("/login")
+  return req.isAuthenticated() ? next() : res.redirect('/login')
 }
 
-module.exports = passportRouter;
+module.exports = passportRouter
